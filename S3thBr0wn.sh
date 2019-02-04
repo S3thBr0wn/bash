653b5861ef47a2f292331f9e75497887643d3476
S3thBr0wn <sdbrown280@gmail.com>
2019-02-04 08:46:55 -0800

    SDB

---
+++ S3thBr0wn.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username  S3thBr0wn;
+git config --global user.name  S3thBr0wn;
+git config --global user.user S3thBr0wn;
+git config --global user.email sdbrown280@gmail.com;
+git config --list;
